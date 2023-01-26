2824316ca6e95ab6dfb216bc7c8d99a32a1bd61f
Jawid <41361157+infjay@users.noreply.github.com>
2023-01-26 14:09:14 +0100

    bugfixed misspelled

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import { useState , useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, formHelperTextClasses } from "@mui/material"
-import { Videos , ChannelCard } from "./"
-import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  const { id } = useParams();
-  
-  const [channelDetail, setChannelDetail] = useState(null)
-  const [videos, setVideos] = useState([])
- 
-  useEffect(()=> {
-    fetchFromAPI(`channels?part="snippet&id=${id}`)
-      .then((data)=> setChannelDetail(data?.items[0]))
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
 
-    fetchFromAPI(`search?channelId=${id}part="snippet&order=date`)
-      .then((data)=> setVideos(data?.items))
-  },[id])
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
   return (
     <Box minHeight="95vh">
       <Box>
-        <div style={{background:'linear-gradient(90deg,rgba(14,15,0,1) 0%, rgba(255,30,10,1)33%, rgba(0,255,231,1) 71%)',
-        zIndex:10,
-        height:'300px'  
-      }} 
-     />
-          <ChannelCard channelDetail={channelDetail} marginTop="-120px"  />
+        <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
       </Box>
-        <Box display="flex" p="2">
-          <Box sx={{ mr: { sm:"100px" } }} >
-            <Videos videos={videos} />
-          </Box>
-        </Box>
+      <Box p={2} display="flex">
+      <Box sx={{ mr: { sm: '100px' } }}/>
+        <Videos videos={videos} />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ChannelDetail
+export default ChannelDetail;
